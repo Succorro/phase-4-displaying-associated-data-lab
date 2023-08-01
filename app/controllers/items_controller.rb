d638567879c1298c8ccd0c2e0817613184306c0a
Steven Gutierrez <stevogbmv@gmail.com>
2023-07-31 19:08:06 -0700

    commit tree c703a8cdb58f5f2d91549fc2802268f2227b9e6a

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,5 @@
 class ItemsController < ApplicationController
+    def index 
+        render json: Item.all, only: [:id,:name,:description,:price ], include: {user: {except: [:created_at, :updated_at]}}
+    end 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,6 @@
 class UsersController < ApplicationController
+    def show
+        user = User.find_by(id: params[:id])
+        render json: user, except: [:created_at, :updated_at], include: {items: {only: [:id, :name, :description, :price]}}
+    end
 end
